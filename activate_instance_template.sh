ad6203a457524818f4a353de40d6a15701e43444
Omir-Hosny <omir.hosny1212@gmail.com>
2025-12-04 05:57:26 +0200

    add mig zones

--- activate_instance_template.sh
+++ activate_instance_template.sh
@@ -3,9 +3,9 @@ template_name='dev-frontend-'
 template_name+=$1 
 gcloud compute instance-groups managed set-instance-template dev-frontend-instance-group \
                     --template=$template_name \
-                    --zone=me-central1
+                    --zone=me-central1-a
 
 gcloud compute instance-groups managed rolling-action start-update dev-frontend-instance-group \
                     --max-unavailable=0\
-                    --zone=me-central1\
+                    --zone=me-central1-a\
                     --version=template=$template_name
