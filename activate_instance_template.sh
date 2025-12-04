67df667864807263571c8e998b3be7d1a816ad6b
Omir-Hosny <omir.hosny1212@gmail.com>
2025-12-04 06:08:39 +0200

    change instance group name to match

--- activate_instance_template.sh
+++ activate_instance_template.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 template_name='dev-frontend-'
 template_name+=$1 
-gcloud compute instance-groups managed set-instance-template dev-frontend-instance-group \
+gcloud compute instance-groups managed set-instance-template instance-group-1 \
                     --template=$template_name \
                     --zone=me-central1-a
 
-gcloud compute instance-groups managed rolling-action start-update dev-frontend-instance-group \
+gcloud compute instance-groups managed rolling-action start-update instance-group-1 \
                     --max-unavailable=0\
                     --zone=me-central1-a\
                     --version=template=$template_name
